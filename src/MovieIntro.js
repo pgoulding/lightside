5638938d995e8d523555864dcde136a65d069111
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-02 17:16:56 -0600

    show film opening crawl

--- src/MovieIntro.js
+++ src/MovieIntro.js
@@ -3,11 +3,7 @@ import React from 'react'
 const MovieIntro = ({ films }) => {
   
   const findEpisode = (specificMovie) => {
-    if(specificMovie) {
-      return films.find(movie => movie.episode_id === specificMovie)
-    } else {
-      return films.find(movie => movie.episode_id === Math.floor(Math.random() * (7 - 1 + 1)) + 1);
-    }
+      return films.find(movie => movie.episode_id === specificMovie || Math.floor(Math.random() * (7 - 1 + 1)) + 1);
   } 
   
   return (

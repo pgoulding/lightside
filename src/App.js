fa591b655b79d477fafd1b31c50cc39dd6dd7bd3
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-02 11:36:26 -0600

    App and test setup

--- src/App.js
+++ src/App.js
@@ -11,18 +11,36 @@ export class App extends Component {
   constructor () {
     super()
     this.state = {
+      category:'films',
+      film:'',
+      renderedPage:'',
+      pageNumber: null
+    }
+  }
 
+  componentDidMount(){
+    const url = `https://swapi.co/api/${this.state.category}/?page=${this.state.pageNumber}`
+
+  }
+
+  renderPage = () => {
+    if(this.state.renderedPage === 'intro') {
+      return <MovieIntro films={this.state.films} />
+    } else if (this.state.renderedPage === 'home') {
+      return 
+    } else {
+      return <h3>NOTHING HERE</h3>
     }
   }
 
   render() {
     return (
-      <div>
-        
-      </div>
+      <main className="App"> 
+        { this.renderPage() }
+      </main>
     )
   }
-  
+
 }
 
 export default App

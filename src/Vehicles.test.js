4e2536d3484ed7320d4314e390f976b5a6c6ed26
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-02 11:37:17 -0600

    setup tests

---
+++ src/Vehicles.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+import { shallow } from 'enzyme'
+import MovieIntro from './Planets';
+// import testData from './test-data/film-data'
+
+describe('Planets', () => {
+
+
+  beforeEach(() => {
+
+  })
+
+  it('should do stuff', () => {
+    console.log('do stuff')
+  })
+
+})

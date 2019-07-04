04ed56b94ae4f45be6b3be20d75b9aa1ecff7a3b
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-04 16:52:47 -0600

    add all the info to appear on card, and organized it neater

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import faveInactive from '../images/002-prize.svg';
+// import faveInactive from '../images/002-prize.svg';
 import './Card.scss'
 
 const Card = ({ data }) => {
+console.log(data)
+
     return (
         <article className='card'>
             <h2>Name: {data.name}</h2>
-            <h3>{data.birth_year && `Birth Year: ${data.birth_year}` || data.terrain && `Terrain: ${data.terrain}`}</h3>
-            <h3>Species:</h3>
-            <h3>Language:</h3>
-            <h3>Population</h3>
+            <h3>{
+                (data.birth_year && `Birth Year: ${data.birth_year}` )
+                || (data.terrain && `Terrain: ${data.terrain}`) 
+                || (data.model && `Model: ${data.model}`)
+                }</h3>
+            <h3>{
+                (data.gender && `Gender: ${data.gender}` )
+                || (data.diameter && `Diameter: ${data.diameter}`)
+                || (data.vehicle_class && `Class: ${data.vehicle_class}`)
+                }</h3>
+            <h3>{ 
+                (data.height && `Height: ${data.height}`) 
+                || (data.population && `Population: ${data.population}`)
+                || (data.passengers && `Passengers: ${data.passengers}`)
+                || ''}</h3>
+            <h3>{ 
+                (data.eye_color && `Eye Color: ${data.eye_color}` )
+                || (data.climate && `Climate: ${data.climate}` )
+                || ''}</h3>
         </article>
     )
 }

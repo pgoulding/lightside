2a35e4e51739f23ec1239b15865f15890ac18e33
Taylor Jordan <taylornoel418@icloud.com>
2019-07-05 17:41:46 -0600

    Add favIcons to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import faveInactive from '../images/002-prize.svg';
 import './Card.scss'
+import favInactive from '../images/001-lightsaber.svg';
+import favActive from '../images/002-game.svg';
 
-const Card = ({ data }) => {
+const Card = ({ data, isFavorited }) => {
     return (
         <article className='card'>
-            <h2>Name: {data.name}</h2>
+        <img src={isFavorited ? favActive : favInactive} className='favIcon' alt='' />
+            <h2>{data.name}</h2>
             <h3>{data.birth_year && `Birth Year: ${data.birth_year}` || data.terrain && `Terrain: ${data.terrain}` || data.model && `Model: ${data.model}`}</h3>
             <h3>{data.gender && `Gender: ${data.gender}` || data.diameter && `Diameter: ${data.diameter}` || data.class && `Class: ${data.class}`}</h3>
             <h3>{data.height && `Height: ${data.height}` || data.population && `Population: ${data.population}` || data.passengers && `Passengers: ${data.passengers}`}</h3>
--- src/components/Container.js
+++ src/components/Container.js
@@ -2,16 +2,44 @@ import React from 'react';
 import Card from './Card'
 import './Container.scss'
 
-const Container = ({ data }) => {
-    let swData = data.map(ele => {
-        return <Card data={ele}/>
-    })
-    console.log(swData)
-    return (
-        <section className='cardContainer'>
-            { swData }
-        </section>
-    )
+
+
+
+
+class Container extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isFavorited: false,
+        }
+    }
+
+    toggleFavorite = () => {
+        this.setState({ isFavorited: !this.state.isFavorited})
+    }
+
+    render() {
+        let swData = data.map(ele => {
+            return <Card toggleFavorite={this.toggleFavorite} data={ele}/>
+        })
+        return (
+            <section className='cardContainer'>
+                {swData}
+            </section>
+        )
+    }
 }
+
+// const Container = ({ data }) => {
+//     let swData = data.map(ele => {
+//         return <Card data={ele}/>
+//     })
+//     console.log(swData)
+//     return (
+//         <section className='cardContainer'>
+//             { swData }
+//         </section>
+//     )
+// }
 
 export default Container;

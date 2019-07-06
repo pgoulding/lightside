ea074743a3a3ce2973cdaa386e445bbec0bec6cd
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-06 12:27:09 -0600

    sorting data on fetch call

---
+++ src/components/sortData.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const sortData = (fetchData, category) => {
+  console.log(fetchData)
+  if(category === 'people') {
+    console.log('people category')
+    return fetchData.map(person => {
+      return {
+        name: person.name, 
+        "birth year": person.birth_year, 
+        gender: person.gender, 
+        height:person.height,
+        "eye color": person.eye_color,
+        isFavorited: false
+      }
+    })
+  } else if (category === 'planets') {
+    return fetchData.map(planet => {
+      return {
+        name: planet.name, 
+        diameter: planet.diameter, 
+        population: planet.population, climate: planet.climate,
+        isFavorited: false
+      }
+    })
+  } else if (category === 'vehicles') {
+    return fetchData.map(vehicle => {
+      return {
+        name: vehicle.name,
+        "class": vehicle.vehicle_class,
+        model: vehicle.model,
+        passengers: vehicle.passengers,
+        isFavorited: false
+      }
+    })
+  }
+}
+
+export default sortData

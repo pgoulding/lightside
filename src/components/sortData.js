94787ef391a98f5b4c22f075b57051b356df95f6
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-09 14:36:28 -0600

    changed strings to variable names

--- src/components/sortData.js
+++ src/components/sortData.js
@@ -1,19 +1,17 @@
 import React from 'react'
 
 const sortData = (fetchData, category) => {
-  console.log(fetchData)
   if(category === 'people') {
     console.log('people category')
     return fetchData.map(person => {
       return {
         name: person.name, 
-        "birth year": person.birth_year, 
+        birth_year: person.birth_year, 
         gender: person.gender, 
         height:person.height,
-        "eye color": person.eye_color,
+        eye_color: person.eye_color,
         homeworld: person.homeworld,
         species: person.species,
-        "home world population": person.homeworld,
         id: person.created
       }
     })
@@ -32,7 +30,7 @@ const sortData = (fetchData, category) => {
     return fetchData.map(vehicle => {
       return {
         name: vehicle.name,
-        "class": vehicle.vehicle_class,
+        vehicle_class: vehicle.vehicle_class,
         model: vehicle.model,
         passengers: vehicle.passengers,
         id: vehicle.created

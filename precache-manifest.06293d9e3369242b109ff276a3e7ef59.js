a865aafb814826fd3ab8f6bf85014addb2bbeddc
Patrick Goulding <patrickgoulding@gmail.com>
2019-07-15 13:14:58 -0600

    Updates

---
+++ precache-manifest.06293d9e3369242b109ff276a3e7ef59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25983362a1c1c74c0f0367535c61a0e",
+    "url": "/lightside/index.html"
+  },
+  {
+    "revision": "da4a0f398dfaf9d36c44",
+    "url": "/lightside/static/css/2.d1f26340.chunk.css"
+  },
+  {
+    "revision": "53c9ad98ff13004de509",
+    "url": "/lightside/static/css/main.65abd032.chunk.css"
+  },
+  {
+    "revision": "da4a0f398dfaf9d36c44",
+    "url": "/lightside/static/js/2.fec0a9bb.chunk.js"
+  },
+  {
+    "revision": "53c9ad98ff13004de509",
+    "url": "/lightside/static/js/main.c759fe19.chunk.js"
+  },
+  {
+    "revision": "ecdc0bcbd4ae9fd720f2",
+    "url": "/lightside/static/js/runtime~main.30f30001.js"
+  },
+  {
+    "revision": "7176f8c090f57e9e69eedba7b5c41d4f",
+    "url": "/lightside/static/media/001-lightsaber.7176f8c0.svg"
+  },
+  {
+    "revision": "be0942c6b6ee514d5f31ae8ec84e249e",
+    "url": "/lightside/static/media/002-game.be0942c6.svg"
+  },
+  {
+    "revision": "9c3db68b314426a8990cfd42ae8df59d",
+    "url": "/lightside/static/media/002-star-wars.9c3db68b.svg"
+  },
+  {
+    "revision": "ced68ff827c257753f0fc0f929e2c6bb",
+    "url": "/lightside/static/media/006-human.ced68ff8.svg"
+  },
+  {
+    "revision": "277ddc1a85280cf87cdfff1d82492ce5",
+    "url": "/lightside/static/media/007-universe.277ddc1a.svg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/lightside/static/media/nasa-Q1p7bh3SHj8-unsplash.67af05e1.jpg"
+  }
+]);
